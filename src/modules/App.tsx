cf604d445b7d67eda02081ae2ed70b094b18682c
Wanchai Nupinit <wanchai.23.chai@gmail.com>
2020-02-26 17:04:19 +0700

    fix

--- src/modules/App.tsx
+++ src/modules/App.tsx
@@ -1,28 +1,6 @@
 import React from 'react';
 import logo from '../asset/logo.svg';
 import '../style/App.css';
-<<<<<<< Updated upstream
-
-const App: React.FC = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-					วันชัย หนูพินิช
-        </p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-				</a>
-			</header>
-		</div>
-	);
-=======
 const Window = window as any
 const liff = Window.liff
 export default class App extends React.Component<any, any> {
@@ -96,7 +74,4 @@ export default class App extends React.Component<any, any> {
 			</React.Fragment>
 		)
 	}
->>>>>>> Stashed changes
 }
-
-export default App;
