a1d16f26ac409216ca4a2a1710bc8d4648a3f41c
Wanchai Nupinit <wanchai.23.chai@gmail.com>
2020-02-26 10:41:37 +0700

    edit github page not show

---
+++ docs/precache-manifest.72244fe8913b710d44e3850c6d07056c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "807dd15499b73fa400db9a056759331a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eea256e703a22a2811a1",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9c4c07642f61988473e4",
+    "url": "/static/css/main.89e91970.chunk.css"
+  },
+  {
+    "revision": "eea256e703a22a2811a1",
+    "url": "/static/js/2.e50bc5ac.chunk.js"
+  },
+  {
+    "revision": "9c4c07642f61988473e4",
+    "url": "/static/js/main.967559ff.chunk.js"
+  },
+  {
+    "revision": "92f416338fa5b92447d6",
+    "url": "/static/js/runtime-main.79fbf0b5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "23817162a8f9a5cafdac91f1bb4e8a0b",
+    "url": "/static/media/scg-logo.23817162.png"
+  }
+]);
