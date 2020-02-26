668b69c7773e1999152d5c864e100fc5147938c7
Wanchai Nupinit <wanchai.23.chai@gmail.com>
2020-02-26 10:27:30 +0700

    add build src for github page

---
+++ docs/precache-manifest.18ed4a6e85e13d3f3e56e9363de90829.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "821566906664c5c6ea91b5432a468017",
+    "url": "baseStructureReactTS/web/index.html"
+  },
+  {
+    "revision": "eea256e703a22a2811a1",
+    "url": "baseStructureReactTS/web/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e6ec9120ed2b9ab08ad8",
+    "url": "baseStructureReactTS/web/static/css/main.89e91970.chunk.css"
+  },
+  {
+    "revision": "eea256e703a22a2811a1",
+    "url": "baseStructureReactTS/web/static/js/2.e50bc5ac.chunk.js"
+  },
+  {
+    "revision": "e6ec9120ed2b9ab08ad8",
+    "url": "baseStructureReactTS/web/static/js/main.259bb324.chunk.js"
+  },
+  {
+    "revision": "30242372eade9fd5c226",
+    "url": "baseStructureReactTS/web/static/js/runtime-main.107e0af9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "baseStructureReactTS/web/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "23817162a8f9a5cafdac91f1bb4e8a0b",
+    "url": "baseStructureReactTS/web/static/media/scg-logo.23817162.png"
+  }
+]);
