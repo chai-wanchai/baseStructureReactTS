f5896c5339b9e21d5aefd4d64f0dc215f067072a
Wanchai Nupinit <wanchai.23.chai@gmail.com>
2020-02-28 13:47:25 +0700

    Updates

---
+++ precache-manifest.a596e0382b0aa37e465f648b59f581e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83920a2b9ffc528aa62821a87041dd3",
+    "url": "/baseStructureReactTS/index.html"
+  },
+  {
+    "revision": "ebdf071531f5476664fc",
+    "url": "/baseStructureReactTS/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "cf7f3bb81c8a7912eab0",
+    "url": "/baseStructureReactTS/static/css/main.33213e63.chunk.css"
+  },
+  {
+    "revision": "ebdf071531f5476664fc",
+    "url": "/baseStructureReactTS/static/js/2.6e830ec7.chunk.js"
+  },
+  {
+    "revision": "cf7f3bb81c8a7912eab0",
+    "url": "/baseStructureReactTS/static/js/main.6f88ba41.chunk.js"
+  },
+  {
+    "revision": "48c59e1e7422f6bac3a4",
+    "url": "/baseStructureReactTS/static/js/runtime-main.770c8893.js"
+  },
+  {
+    "revision": "23817162a8f9a5cafdac91f1bb4e8a0b",
+    "url": "/baseStructureReactTS/static/media/scg-logo.23817162.png"
+  }
+]);
